adc417f1e5f7a4f10aa32a7d3614acc25777b53b
matteoKuama <matteo@kuama.net>
2021-09-13 17:08:32 +0200

    Changing WifiMonitor exposing method to Flow.

--- src/main/java/net/kuama/wifiMonitor/WifiMonitor.kt
+++ src/main/java/net/kuama/wifiMonitor/WifiMonitor.kt
@@ -5,15 +5,22 @@ import android.content.Context
 import android.net.wifi.WifiInfo
 import android.net.wifi.WifiManager
 import android.os.Build
+import android.util.Log
 import androidx.core.content.ContextCompat
 import androidx.core.content.PermissionChecker.PERMISSION_GRANTED
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
 import net.kuama.wifiMonitor.data.WifiStatus
+import net.kuama.wifiMonitor.data.WifiStatus.State
 import net.kuama.wifiMonitor.implementation.AndroidQWifiListener
 import net.kuama.wifiMonitor.implementation.BeforeAndroidQWifiListener
 
 class WifiMonitor(context: Context) {
+    companion object {
+        private val TAG = WifiMonitor::class.simpleName
+    }
 
     private val listener = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
         AndroidQWifiListener(context)
@@ -25,27 +32,6 @@ class WifiMonitor(context: Context) {
     private val wifiManager: WifiManager =
         context.applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
 
-    /**
-     * Whether we received at least a Wi-Fi change status. When false, we cannot say we are connected
-     */
-    private var didReceiveChange = false
-
-    /**
-     * Stop listening to Wi-Fi changes
-     */
-    fun stop() = listener.stop()
-
-    /**
-     * Triggers the on change callback whenever the Wi-Fi changes its status
-     */
-    suspend fun observe(onChange: (WifiStatus) -> Unit) =
-        withContext(Dispatchers.Default) {
-            listener.start {
-                didReceiveChange = true
-                onChange(info)
-            }
-        }
-
     private val state: Int
         get() = wifiManager.wifiState
 
@@ -53,38 +39,14 @@ class WifiMonitor(context: Context) {
         get() = wifiManager.connectionInfo
 
     private val band: WifiStatus.NetworkBand
-        get() = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+        get() =
             if (connectionInfo.frequency > 3000) {
                 WifiStatus.NetworkBand.WIFI_5_GHZ
             } else {
                 WifiStatus.NetworkBand.WIFI_2_4_GHZ
             }
-        } else {
-            WifiStatus.NetworkBand.UNKNOWN
-        }
 
-    /**
-     * Holds the current information on the Wi-Fi connection.
-     */
-    val info: WifiStatus
-        get() = if (didReceiveChange) {
-            when (state) {
-                WifiManager.WIFI_STATE_DISABLED, WifiManager.WIFI_STATE_DISABLING -> WifiStatus(
-                    WifiStatus.State.DISCONNECTED
-                )
-                WifiManager.WIFI_STATE_ENABLED -> WifiStatus(
-                    state = if (isFineLocationAccessGranted) WifiStatus.State.CONNECTED else WifiStatus.State.CONNECTED_MISSING_FINE_LOCATION_PERMISSION,
-                    ssid = connectionInfo.ssid,
-                    bssid = connectionInfo.bssid,
-                    band = band,
-                    rssi = connectionInfo.rssi
-                )
-                WifiManager.WIFI_STATE_ENABLING -> WifiStatus(WifiStatus.State.ENABLING)
-                else -> WifiStatus(WifiStatus.State.UNKNOWN)
-            }
-        } else {
-            WifiStatus(WifiStatus.State.UNKNOWN)
-        }
+    private var wifiStatus: WifiStatus = WifiStatus(State.UNKNOWN)
 
     /**
      * True if the user granted access to [Manifest.permission.ACCESS_FINE_LOCATION]
@@ -93,4 +55,45 @@ class WifiMonitor(context: Context) {
         context,
         Manifest.permission.ACCESS_FINE_LOCATION
     ) == PERMISSION_GRANTED
+
+    /**
+     * Start listening to Wi-fi changes exposing a flow of WifiStatus.
+     * It can throw an exception when the channel publishing on a channel that is closed
+     */
+    @ExperimentalCoroutinesApi
+    suspend fun start(): Flow<WifiStatus> = callbackFlow {
+        listener.start {
+            // Update wifiStatus value accordingly with the new state
+            wifiStatus = when (state) {
+                WifiManager.WIFI_STATE_DISABLED, WifiManager.WIFI_STATE_DISABLING -> WifiStatus(
+                    State.DISCONNECTED
+                )
+                WifiManager.WIFI_STATE_ENABLED -> WifiStatus(
+                    state = if (isFineLocationAccessGranted) State.CONNECTED else State.CONNECTED_MISSING_FINE_LOCATION_PERMISSION,
+                    ssid = connectionInfo.ssid,
+                    bssid = connectionInfo.bssid,
+                    band = band,
+                    rssi = connectionInfo.rssi
+                )
+                WifiManager.WIFI_STATE_ENABLING -> WifiStatus(State.ENABLING)
+                else -> WifiStatus(State.UNKNOWN)
+            }
+            // Surrounding with try-catch because the channel may be closed
+            try {
+                // Publish value to the Flow
+                channel.offer(wifiStatus)
+            } catch (e: Exception) {
+                Log.d(TAG, "Send channel is closed, it wasn't possible to publish a new value", e)
+            }
+        }
+        awaitClose {
+            wifiStatus = WifiStatus(State.UNKNOWN)
+            stop()
+        }
+    }
+
+    /**
+     * Stop listening to Wi-Fi changes
+     */
+    fun stop() = listener.stop()
 }
