462140cb2168470b4bd20867e3a111d01c03d2a6
matteoKuama <matteo@kuama.net>
2022-05-06 10:50:35 +0200

    Upgrade to kotlin 1.6.21 Upgrade gradle to 7.0.4 Change flow publisher type to WifiInfo? Remove unused res

--- src/main/java/net/kuama/wifiMonitor/implementation/AndroidQWifiListener.kt
+++ src/main/java/net/kuama/wifiMonitor/implementation/AndroidQWifiListener.kt
@@ -5,6 +5,8 @@ import android.content.Context
 import android.net.ConnectivityManager
 import android.net.Network
 import android.net.NetworkCapabilities
+import android.net.NetworkRequest
+import android.net.wifi.WifiInfo
 import android.os.Build
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.channels.awaitClose
@@ -24,19 +26,25 @@ import net.kuama.wifiMonitor.WifiListener
 @TargetApi(Build.VERSION_CODES.Q)
 internal class AndroidQWifiListener : WifiListener {
     @OptIn(ExperimentalCoroutinesApi::class)
-    override fun listen(context: Context): Flow<Unit> = callbackFlow {
+    override fun listen(context: Context): Flow<WifiInfo?> = callbackFlow {
         val callback = object : ConnectivityManager.NetworkCallback() {
             override fun onCapabilitiesChanged(network: Network, networkCapabilities: NetworkCapabilities) {
                 super.onCapabilitiesChanged(network, networkCapabilities)
+
+                val wifiInfo = networkCapabilities.transportInfo as WifiInfo
                 @Suppress("BlockingMethodInNonBlockingContext")
-                trySendBlocking(Unit)
+                trySendBlocking(wifiInfo)
             }
         }
+
+        val request = NetworkRequest.Builder()
+            .addTransportType(NetworkCapabilities.TRANSPORT_WIFI)
+            .build()
 
         val connectivityManager = context.applicationContext.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
 
         // Register the callback on network changes.
-        connectivityManager.registerDefaultNetworkCallback(callback)
+        connectivityManager.registerNetworkCallback(request, callback)
 
         // Wait for flow cancellation, then unregister the callback.
         awaitClose {
