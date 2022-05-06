59e7d694e4f1f2c3453e2eb86e7b3e3fa8855f2a
Leonardo Nodari <me@leonardonodari.it>
2022-05-06 14:58:17 +0200

    Android 12: Fix retrieval without using WiFiManager.getConnectionInfo

--- src/main/java/net/kuama/wifiMonitor/implementation/AndroidQWifiListener.kt
+++ src/main/java/net/kuama/wifiMonitor/implementation/AndroidQWifiListener.kt
@@ -5,8 +5,8 @@ import android.content.Context
 import android.net.ConnectivityManager
 import android.net.Network
 import android.net.NetworkCapabilities
-import android.net.NetworkRequest
 import android.net.wifi.WifiInfo
+import android.net.wifi.WifiManager
 import android.os.Build
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.channels.awaitClose
@@ -24,31 +24,41 @@ import net.kuama.wifiMonitor.WifiListener
  * From now on, to observe connectivity changes we should register a [ConnectivityManager.NetworkCallback] implementation.
  */
 @TargetApi(Build.VERSION_CODES.Q)
-internal class AndroidQWifiListener : WifiListener {
+internal class AndroidQWifiListener(private val wifiManager: WifiManager) : WifiListener {
     @OptIn(ExperimentalCoroutinesApi::class)
     override fun listen(context: Context): Flow<WifiInfo?> = callbackFlow {
-        val callback = object : ConnectivityManager.NetworkCallback() {
-            override fun onCapabilitiesChanged(network: Network, networkCapabilities: NetworkCapabilities) {
-                super.onCapabilitiesChanged(network, networkCapabilities)
+        // Android 12 requires a new flag to receive SSID data, but flag-based constructor has been introduced only in SDK 31.
+        val networkCallback = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S)
+            object : ConnectivityManager.NetworkCallback(FLAG_INCLUDE_LOCATION_INFO) {
+                override fun onCapabilitiesChanged(network: Network, networkCapabilities: NetworkCapabilities) {
+                    super.onCapabilitiesChanged(network, networkCapabilities)
 
-                val wifiInfo = networkCapabilities.transportInfo as WifiInfo
-                @Suppress("BlockingMethodInNonBlockingContext")
-                trySendBlocking(wifiInfo)
+                    trySendBlocking(networkCapabilities.transportInfo as? WifiInfo?)
+                }
+            } else
+            object : ConnectivityManager.NetworkCallback() {
+                override fun onCapabilitiesChanged(network: Network, networkCapabilities: NetworkCapabilities) {
+                    super.onCapabilitiesChanged(network, networkCapabilities)
+
+                    // There's a weird behavior in SDK 30 where transportInfo is null even when connected to the WiFi.
+                    // Fallback to soon-to-deprecate connectionInfo if we're connected to a WiFi network.
+                    @Suppress("DEPRECATION")
+                    val wifiInfo =
+                        (networkCapabilities.transportInfo as? WifiInfo?)
+                            ?: if (wifiManager.wifiState == WifiManager.WIFI_STATE_ENABLED) wifiManager.connectionInfo else null
+
+                    trySendBlocking(wifiInfo)
+                }
             }
-        }
-
-        val request = NetworkRequest.Builder()
-            .addTransportType(NetworkCapabilities.TRANSPORT_WIFI)
-            .build()
 
         val connectivityManager = context.applicationContext.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
 
         // Register the callback on network changes.
-        connectivityManager.registerNetworkCallback(request, callback)
+        connectivityManager.registerDefaultNetworkCallback(networkCallback)
 
         // Wait for flow cancellation, then unregister the callback.
         awaitClose {
-            connectivityManager.unregisterNetworkCallback(callback)
+            connectivityManager.unregisterNetworkCallback(networkCallback)
         }
     }
 }
